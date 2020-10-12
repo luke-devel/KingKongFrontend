a8a7a6848f125699dfeac949ba3c37e57d352ed9
Luke <luke.olson499@gmail.com>
2020-10-12 17:01:01 -0500

    Admin page added with AdminBody component for future use

---
+++ pages/admin.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import AdminBody from "../components/AdminBody";
+
+export default function Admin() {
+  return (
+    <div>
+      <Header />
+      <AdminBody/>
+      <Footer />
+    </div>
+  );
+}
