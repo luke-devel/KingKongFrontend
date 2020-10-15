c958d1f9dcc33d0760c24a6122a507db7ee100bd
luke <luke@luke.localdomain>
2020-10-15 08:14:50 -0500

    register now sends states to axios which then talks to the secondary express server where our database is

--- pages/api/register/index.js
+++ pages/api/register/index.js
@@ -1,26 +1,28 @@
-// import bcrypt from "bcrypt";
-// import db from "../models";
+import axios from 'axios';
+import bcrypt from "bcrypt";
 
 export default async function (req, res) {
-  let user;
   try {
-    console.log(req.body);
-    // const hash = await bcrypt.hash(req.body.password, 10);
-    // user = await db.user.create({
-    //   name: req.body.name,
-    //   username: req.body.username,
-    //   email: req.body.email,
-    //   password: hash,
-    // });
-    console.log('hi');
-    res.json('hello')
-    res.status(201);
-  } catch (err) {
-    // console.log("err in api/regiter/index.js sequelize,", err)
-    console.log("err")
-
+    // console.log(req.body);
+    const hash = await bcrypt.hash(req.body.password, 10);
+    const response = await axios({
+      method: "POST",
+      url: `${process.env.REQ_URL}/registeruser`,
+      headers: {
+        name: req.body.name,
+        phone: req.body.phone,
+        email: req.body.email,
+        password: hash,
+      },
+    });
+    // console.log(response); 
     res.status(200);
+    res.json('hi')
+    res.end();
+  } catch (err) {
+    console.log("err in api/regiter/index.js sequelize,", err)
+    // res.json(JSON.stringify(err))
+    res.status(400);
+    res.end();
   }
-  res.end(JSON.stringify(user));
-
 }
