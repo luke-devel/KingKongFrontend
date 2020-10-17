e6b769a4c267d20e55813809a97bf56ccbba0b6d
Luke <luke.olson499@gmail.com>
2020-10-17 13:41:49 -0500

    img tags edited for nextjs

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
       <nav>
         <Link href="/">
           <a className="logo">
-            <img src="img/logo.png" />
+            <img src="/img/logo.png" />
           </a>
         </Link>
 
