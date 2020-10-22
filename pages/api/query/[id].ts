9e6a66c98c1b250adfcb10ddfc6c99757c72417b
Luke <luke.olson499@gmail.com>
2020-10-21 19:42:51 -0500

    user panel populates with added ftp servers

---
+++ pages/api/query/[id].ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import Axios from "axios";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { id } = req.query;
+  console.log('yeah we got i t', id);
+  // if (!req.dat.id || !req.headers.email) {
+  //   res.status(405).json("no headers");
+  // }
+  const backendRes = await Axios(`${process.env.REQ_URL}/api/query/${id}`, {
+    method: "GET",
+    headers:{
+      Accept: "application/json"
+    }
+  });
+  console.log(backendRes.status);
+
+  backendRes.status === 253 && res.status(253).json(JSON.parse(backendRes.data));
+};
