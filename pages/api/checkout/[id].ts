eb8d721b44e3f7b2a57c079a71d392b883d4e329
Luke <luke.olson499@gmail.com>
2020-10-25 13:59:11 -0500

    test stripe2

--- pages/api/checkout/[id].ts
+++ pages/api/checkout/[id].ts
@@ -14,17 +14,23 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const { payment_status } = await stripe.checkout.sessions.retrieve(
       id as string
     );
+    console.log(payment_status);
     if (payment_status === "paid") {
-      const addPaidUserRes = await Axios(`${process.env.REQ_URL}/api/checkauth`, {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-        },
-        data: {
-          userToken: req.cookies.usertoken,
-        },
-      });
+      console.log("payment status is paid");
+      const addPaidUserRes = await Axios(
+        `${process.env.REQ_URL}/api/checkauth`,
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+          },
+          data: {
+            userToken: req.cookies.usertoken,
+          },
+        }
+      );
+      console.log(addPaidUserRes);
+      res.status(200).json({ payment_status });
     }
-    res.status(200).json({ payment_status });
   }
 };
