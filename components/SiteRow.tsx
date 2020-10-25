64bbcf8a2182b1e42757cc8dbe2ef2d24d49ca63
Luke <luke.olson499@gmail.com>
2020-10-25 18:59:59 -0500

    user can now add site to backup status on user panel. routing with backend server

--- components/SiteRow.tsx
+++ components/SiteRow.tsx
@@ -1,4 +1,18 @@
-export default function SiteRow({ siteName, siteLink, serverID }) {
+import Axios from "axios";
+
+export default function SiteRow({ count, siteName, siteLink, serverID }) {
+  const handleBackup = async (serverID, ftpListCount) => {
+    // e.preventdefault()
+    const addBackupRes = await Axios(`/api/addbackup`, {
+      method: "POST",
+      data: {
+        serverRowID: serverID,
+        ftpListCount: ftpListCount,
+      },
+    });
+    console.log('RES: ', addBackupRes);
+  };
+
   return (
     <tr>
       <td>
@@ -8,7 +22,11 @@ export default function SiteRow({ siteName, siteLink, serverID }) {
         <a href={siteLink}>{siteLink}</a>
       </td>
       <td className="imgtd">
-        <a id={serverID} style={{margin: 0}}>
+        <a
+          id={serverID}
+          style={{ margin: 0 }}
+          onClick={() => handleBackup(serverID, count)}
+        >
           <img
             style={{ cursor: "pointer", position: "absolute", zIndex: 1 }}
             className="tbimg"
@@ -17,7 +35,7 @@ export default function SiteRow({ siteName, siteLink, serverID }) {
         </a>
       </td>
       <td className="imgtd">
-        <a id={serverID} style={{margin: 0}}>
+        <a id={serverID} style={{ margin: 0 }}>
           <img
             onClick={() => console.log(serverID)}
             style={{ cursor: "pointer", position: "absolute", zIndex: 1 }}
