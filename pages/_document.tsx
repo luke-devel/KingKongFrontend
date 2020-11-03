dc052decec58643512782a0a788f786d69eed0e9
luke <luke@luke.localdomain>
2020-11-03 11:26:26 -0600

    responsive for mobile

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=0.41, maximum-scale=1" />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;1,900&display=swap"
             rel="stylesheet"
